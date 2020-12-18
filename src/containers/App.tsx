222fb10c291bb4103e160e97d54190af32901c03
JoaoDavid <joaodavid256+github@gmail.com>
2020-12-18 13:35:32 +0000

    moved to containers folder

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from '../logo.svg';
 import './App.css';
+import createEngine, {
+  DefaultLinkModel,
+  DefaultNodeModel,
+  DiagramModel,
+  DiagramEngine
+} from '@projectstorm/react-diagrams';
+import {CanvasWidget} from '@projectstorm/react-canvas-core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+
 }
+
+type MyProps = {  };
+type MyState = {
+  engine: DiagramEngine,
+  nodes: DefaultNodeModel[]
+};
+
+class App extends React.Component<MyProps, MyState> {
+
+  constructor(props:AppProps) {
+    super(props);
+    this.state = {
+      engine: createEngine(),
+      nodes: []
+    }
+
+    // node 1
+    const node1 = new DefaultNodeModel({
+      name: 'Node 1',
+      color: 'rgb(0,192,255)',
+    });
+    const node3 = new DefaultNodeModel({
+      /*name: 'afawf',
+      color: 'rgb(0,192,255)'*/
+    });
+    node3.setPosition(200,200);
+    node1.setPosition(100, 100);
+    this.state.nodes[0] = node1;
+
+    const node2 = new DefaultNodeModel({
+      name: 'Node 1',
+      color: 'rgb(0,192,255)',
+    });
+    node2.setPosition(100, 100);
+    this.state.nodes[1] = node2;
+
+    let port1 = node1.addOutPort('Out');
+    let port2 = node2.addOutPort('Out');
+
+    // link them and add a label to the link
+    const link = port1.link<DefaultLinkModel>(port2);
+
+    const model = new DiagramModel();
+    model.addAll(node1, node2, link, node3);
+    this.state.engine.setModel(model);
+
+  }
+
+
+  render() {
+
+
+    return (
+        <CanvasWidget className="diagram-container"  engine={this.state.engine} />
+
+
+    );
+  }
+}
+
 
 export default App;
