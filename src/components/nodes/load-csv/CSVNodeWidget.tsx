fb063e58a75088a00226673249cc172fcac9f51d
JoaoDavid <joaodavid256+github@gmail.com>
2020-12-22 00:21:16 +0000

    added csv parsing

--- src/components/nodes/load-csv/CSVNodeWidget.tsx
+++ src/components/nodes/load-csv/CSVNodeWidget.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as _ from 'lodash';
 import { DiagramEngine } from '@projectstorm/react-diagrams-core';
 import { CSVNodeModel } from './CSVNodeModel';
 import {DefaultPortLabel, DefaultPortModel} from '@projectstorm/react-diagrams';
 import classes from './CSV.module.css';
+import Papa from 'papaparse';
 
 /*namespace SB {
     export const Node = styled.div<{ selected: boolean }>`
@@ -43,6 +43,25 @@ export class CSVNodeWidget extends React.Component<CSVNodeProps> {
         return <DefaultPortLabel engine={this.props.engine} port={port} key={port.getID()} />;
     };
 
+    loadCSV = (selectorFiles: FileList) => {
+        if (selectorFiles[0] != null) {
+
+            Papa.parse(selectorFiles[0], {
+                worker: false, // Don't bog down the main thread if its a big file
+                download: true,
+                header: false,
+                complete: function(results:any) {
+                    console.log(results.data);
+                    let num_features = results.data[0].length;//num features
+                    let num_rows = results.data.length;//num entries
+                    console.log("num_features:" + num_features + " num_rows:" + num_rows);
+                }
+            });
+
+        }
+        console.log(selectorFiles);
+    }
+
     //selected={this.props.node.isSelected()}
     render() {
         return (
@@ -52,9 +71,11 @@ export class CSVNodeWidget extends React.Component<CSVNodeProps> {
                 <div className={classes.Title}>
                     <div className={classes.TitleName}>{this.props.node.getOptions().name}</div>
                 </div>
-                <input type="file" accept=".csv" />
+                <input type="file" accept=".csv" onChange={ (e) => this.loadCSV(e.target.files!) } />
                 <div className={classes.Ports}>
-                    <div className={classes.PortsContainer}>{_.map(this.props.node.getOutPorts(), this.generatePort)}</div>
+                    <div className={classes.PortsContainer}>{this.props.node.getOutPorts().map((port) => {
+                        return this.generatePort(port);
+                    })}</div>
                 </div>
             </div>
         );
