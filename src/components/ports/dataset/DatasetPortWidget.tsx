8e200b13d6ea299458c5278a592af40429338ec5
JoaoDavid <joaodavid256+github@gmail.com>
2020-12-27 16:53:11 +0000

    added CSS, removed namespace

--- src/components/ports/dataset/DatasetPortWidget.tsx
+++ src/components/ports/dataset/DatasetPortWidget.tsx
@@ -1,51 +1,27 @@
 import * as React from 'react';
 import { DiagramEngine, PortWidget } from '@projectstorm/react-diagrams-core';
-//import { DefaultPortModel } from '@projectstorm/react-diagrams-defaults';
-import {DatasetPortModel} from "./DatasetPortModel";
-import styled from '@emotion/styled';
+import { DatasetPortModel } from "./DatasetPortModel";
+import classes from './DatasetPort.module.css';
 
 export interface DatasetPortLabelProps {
     port: DatasetPortModel;
     engine: DiagramEngine;
 }
 
-namespace S {
-    export const PortLabel = styled.div`
-		display: flex;
-		margin-top: 1px;
-		align-items: center;
-	`;
-
-    export const Label = styled.div`
-		padding: 0 5px;
-		flex-grow: 1;
-	`;
-
-    export const Port = styled.div`
-		width: 15px;
-		height: 15px;
-		background: rgba(255, 255, 255, 0.1);
-
-		&:hover {
-			background: rgb(192, 255, 0);
-		}
-	`;
-}
-
 export class DatasetPortLabel extends React.Component<DatasetPortLabelProps> {
     render() {
         const port = (
             <PortWidget engine={this.props.engine} port={this.props.port}>
-                <S.Port />
+                <div className={classes.Port} />
             </PortWidget>
         );
-        const label = <S.Label>{this.props.port.getOptions().label}</S.Label>;
+        const label = <div className={classes.Label}>{this.props.port.getOptions().label}</div>;
 
         return (
-            <S.PortLabel>
+            <div className={classes.PortLabel}>
                 {this.props.port.getOptions().in ? port : label}
                 {this.props.port.getOptions().in ? label : port}
-            </S.PortLabel>
+            </div>
         );
     }
 }
