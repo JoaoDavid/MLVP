c3acbfa9eb9a5e676db2f3d9c6677c0e304371eb
JoaoDavid <joaodavid256+github@gmail.com>
2020-12-30 18:05:02 +0000

    organized code

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,91 +1,74 @@
 import React from 'react';
 import './App.css';
-import createEngine, {
-  DefaultLinkModel,
-  DefaultNodeModel,
-  DiagramModel,
-  DiagramEngine
-} from '@projectstorm/react-diagrams';
+import createEngine, { DiagramModel, DiagramEngine } from '@projectstorm/react-diagrams';
 import {CanvasWidget} from '@projectstorm/react-canvas-core';
 import {CSVNodeModel} from "../components/nodes/data/import-dataset/csv/CSVNodeModel";
 import {CSVNodeFactory} from "../components/nodes/data/import-dataset/csv/CSVNodeFactory";
 import {BasePortFactory} from "../components/ports/base/BasePortFactory";
 import {RandomForestNodeFactory} from '../components/nodes/model/random-forest/RandomForestNodeFactory';
 import {RandomForestNodeModel} from '../components/nodes/model/random-forest/RandomForestNodeModel';
+import {CoreNodeModel} from '../components/nodes/core/CoreNode/CoreNodeModel'
 
 interface AppProps {
 
 }
 
-type MyProps = {  };
-type MyState = {
-  engine: DiagramEngine,
-  model: DiagramModel
+type AppState = {
+    engine: DiagramEngine,
+    model: DiagramModel
 };
 
-class App extends React.Component<MyProps, MyState> {
+class App extends React.Component<AppProps, AppState> {
 
-  constructor(props:AppProps) {
-    super(props);
-    this.state = {
-      engine: createEngine(),
-      model: new DiagramModel()
+    private nodes: CoreNodeModel[] = [];
+
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {
+            engine: createEngine(),
+            model: new DiagramModel()
+        }
+        this.registerFactories();
+        this.state.engine.setModel(this.state.model);
+        this.addTestNodes();
     }
-    this.state.engine.getNodeFactories().registerFactory(new CSVNodeFactory()); // i cant figure out why
-    this.state.engine.getNodeFactories().registerFactory(new RandomForestNodeFactory());
-    this.state.engine.getPortFactories().registerFactory(new BasePortFactory());
 
-    // node 1
-    const node1 = new DefaultNodeModel({
-      name: 'Node 1',
-      color: 'rgb(0,192,255)',
+    registerFactories = () => {
+        this.state.engine.getNodeFactories().registerFactory(new CSVNodeFactory());
+        this.state.engine.getNodeFactories().registerFactory(new RandomForestNodeFactory());
+        this.state.engine.getPortFactories().registerFactory(new BasePortFactory());
+    }
 
-    });
+    addTestNodes = () => {
+        let count = 10;
+        //node2.setPosition(100, 100);
+        this.nodes.push(new CSVNodeModel());
+        this.nodes.push(new CSVNodeModel());
+        this.nodes.push(new RandomForestNodeModel());
 
-    node1.setPosition(100, 100);
+        this.nodes.forEach((node: CoreNodeModel) => {
+            this.state.model.addNode(node);
+            node.setPosition(count, count);
+            count += 130;
+        });
+    }
 
-    const node2 = new DefaultNodeModel({
-      name: 'Node 2',
-      color: 'rgb(78,29,212)',
-    });
-    //node2.setPosition(100, 100);
+    addNode = () => {
+        const node = new CSVNodeModel();
+        this.state.model.addNode(node);
+        this.state.engine.repaintCanvas();
+    }
 
-    let port1 = node1.addOutPort('Out');
-    let port12 = node1.addOutPort('Out12');
-    let port132 = node1.addInPort('in');
-    let port2 = node2.addInPort('In');
+    render() {
+        return (
+            <div>
+                <button onClick={this.addNode}>Toggle</button>
+                <CanvasWidget className="diagram-container" engine={this.state.engine}/>
 
-    // link them and add a label to the link
-    //const link = port1.link<DefaultLinkModel>(port2);
-    const node3 = new CSVNodeModel();
-    const node4 = new CSVNodeModel();
-    const node5 = new RandomForestNodeModel();
+            </div>
+        );
+    }
 
-    this.state.model.addAll(node1 , node2, node3, node4, node5);//, link);
-    this.state.engine.setModel(this.state.model);
-
-  }
-
-  addNode = () => {
-    const node = new CSVNodeModel();
-    this.state.model.addNode(node);
-    this.state.engine.repaintCanvas();
-  }
-
-
-  render() {
-
-
-    return (
-        <div>
-          <button onClick={this.addNode}>Toggle</button>
-          <CanvasWidget className="diagram-container"  engine={this.state.engine}  />
-
-        </div>
-
-
-    );
-  }
 }
 
 
