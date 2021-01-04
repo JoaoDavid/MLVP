f2ac55900ee453d93686d4647fa4611aa240c523
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-04 00:43:10 +0000

    height 100% without scroll bar

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
