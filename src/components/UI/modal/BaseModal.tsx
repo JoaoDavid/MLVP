b9fad7e4b4fcd7a18c1eeca6de14c8b3c20f460c
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-04 18:47:51 +0000

    added sample form

--- src/components/UI/modal/BaseModal.tsx
+++ src/components/UI/modal/BaseModal.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Form';
 
 interface BaseModalProps {
-/*
-receber aqui as variaveis para fazer binding com os inputs que meter no modal
- */
+    /*
+    receber aqui as variaveis para fazer binding com os inputs que meter no modal
+     */
     handleClose: () => void;
     handleShow: () => void;
     show: boolean;
@@ -19,7 +20,60 @@ const BaseModal = (props: BaseModalProps) => {
                 <Modal.Header closeButton>
                     <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control placeholder="1234 Main St" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formGridAddress2">
+                            <Form.Label>Address 2</Form.Label>
+                            <Form.Control placeholder="Apartment, studio, or floor" />
+                        </Form.Group>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridCity">
+                                <Form.Label>City</Form.Label>
+                                <Form.Control />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridState">
+                                <Form.Label>State</Form.Label>
+                                <Form.Control as="select" defaultValue="Choose...">
+                                    <option>Choose...</option>
+                                    <option>...</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridZip">
+                                <Form.Label>Zip</Form.Label>
+                                <Form.Control />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Group id="formGridCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    Woohoo, you're reading this text in a modal!
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.handleClose}>
                         Close
