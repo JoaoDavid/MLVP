e8b1ce81a2c1024662be82734652996e8726e4f4
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-11 00:30:40 +0000

    init toggle side bar

--- src/components/UI/side-bar/SideBar.tsx
+++ src/components/UI/side-bar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './SideBar.module.css';
 import Category from "./category/Category";
 import {CategoryConfig, NodeConfig} from "../../nodes/Config";
@@ -9,14 +9,26 @@ interface SideBarProps {
 }
 
 const SideBar = (props: SideBarProps) => {
+    const [isOpen, change] = useState(true);
+    const arrClasses = [classes.SideBar];
+
     const categories: JSX.Element[] = [];
     props.catAndNames.forEach((value, key) => {
         categories.push(
             <Category key={key.category} color={key.color} title={key.category} nodes={value} format={props.format}/>);
     });
-    console.log(props.catAndNames);
+
+    if (isOpen) {
+        arrClasses.push(classes.Open);
+    } else {
+        arrClasses.push(classes.Closed);
+    }
+
     return (
-        <div className={classes.SideBar}>
+        <div className={arrClasses.join(' ')}>
+            <div onClick={()=>{
+                    change(!isOpen);
+            }}>.</div>
             {categories}
         </div>
     )
