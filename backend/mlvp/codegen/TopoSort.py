62f1d81b640c9b99dd38a35a0f48f12f1295a3e5
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-21 15:14:21 +0000

    added list with parent statements

--- backend/mlvp/codegen/TopoSort.py
+++ backend/mlvp/codegen/TopoSort.py
@@ -12,28 +12,30 @@ class TopoSort:
     def __init__(self, json_nodes, json_links):
         self.json_nodes = json_nodes
         self.json_links = json_links
-        self.nodes = {}
+        self.statements = {}
 
     def get_layers(self):
         layers = [[] for _ in range(NUM_NODE_LAYERS)]
-        nodes = {}
         for key, value in self.json_nodes.items():
+            parents = self.__get_parent_statements(key)
             if value['type'] == 'NODE_IMPORT_CSV':
                 ds_type = Csv(file_name=value['fileName'], num_cols=value['numCols'], num_rows=value['numRows'],
                               target=value['columnNames'][-1])
                 layers[0].append(DatasetDeclarationStatement(node_id=key, ds_type=ds_type))
-                self.nodes[key] = ds_type
+                self.statements[key] = ds_type
             elif value['type'] == 'NODE_RANDOM_FOREST':
                 model_type = RandomForest(num_trees=value['numTrees'], criterion=value['criterion'],
                                           max_depth=value['maxDepth'])
-                layers[1].append(ModelTrainStatement(node_id=key, model_type=model_type))
-                self.nodes[key] = model_type
+                layers[1].append(ModelTrainStatement(parents=parents, node_id=key, model_type=model_type))
+                self.statements[key] = model_type
             elif value['type'] == 'NODE_ACCURACY':
-                layers[2].append(ModelAccuracyStatement(node_id=key))
+                layers[2].append(ModelAccuracyStatement(node_id=key, parents=parents))
 
         return layers
 
-    def __parse_links(self, node_id: str):
+    def __get_parent_statements(self, node_id: str):
+        parents = []
         for key, value in self.json_links.items():
             if node_id == value['target']:
-                print("testing")
+                parents.append(self.statements[value['source']])
+        return parents
--- backend/mlvp/statement/Statement.py
+++ backend/mlvp/statement/Statement.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 class Statement(ABC):
 
     @abstractmethod
-    def __init__(self, node_id: str):
+    def __init__(self, node_id: str, parents):
         self.node_id = node_id
+        self.parents = parents
         pass
