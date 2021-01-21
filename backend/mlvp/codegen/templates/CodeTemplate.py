03ea70881191bfd866025a0ff17f352c13e7c82c
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-21 18:52:06 +0000

    fixed random forest parameter typo, added print and toList

--- backend/mlvp/codegen/templates/CodeTemplate.py
+++ backend/mlvp/codegen/templates/CodeTemplate.py
@@ -5,6 +5,9 @@ LOAD_CSV = "{var} = {pandas_var}.read_csv(\'./{file_name}\')\n"
 FEATURES = "{x} = {var}.drop(\'{target}\', axis=1)\n"
 TARGET = "{y} = {var}[\'{target}\']\n"
 
-RANDOM_FOREST_INIT = "{var} = RandomForestClassifier(numTrees={num_trees}, criterion=\"{criterion}\", max_depth={max_depth})\n"
+RANDOM_FOREST_INIT = "{var} = RandomForestClassifier(n_estimators={num_trees}, criterion=\"{criterion}\", max_depth={max_depth})\n"
 MODEL_FIT = "{var}.fit({x}, {y})\n"
 MODEL_PREDICT = "{var} = {clf_var}.predict({x})\n"
+
+SERIES_TO_LIST = "{series_var}.tolist()\n"
+PRINT = "print({content})\n"
