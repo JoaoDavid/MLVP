803c20b1e15e4928f9a55504f4969b95431f9227
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-21 17:21:09 +0000

    writes x and y dataframe based on target column

--- backend/mlvp/codegen/CodeGenerator.py
+++ backend/mlvp/codegen/CodeGenerator.py
@@ -6,6 +6,7 @@ from mlvp.statement.DatasetDeclarationStatement import DatasetDeclarationStateme
 from mlvp.statement.ModelAccuracyStatement import ModelAccuracyStatement
 from mlvp.statement.RandomForestStatement import RandomForestStatement
 
+
 class CodeGenerator:
 
     def __init__(self, links, nodes, name):
@@ -19,6 +20,7 @@ class CodeGenerator:
     def __write_imports(self, libraries):
         for lib in libraries:
             self.outFile.write(lib)
+        self.outFile.write("\n\n")
 
     def generate_code(self):
         layers, libraries = self.topo_sort.get_layers()
@@ -49,11 +51,17 @@ class CodeGenerator:
             print(df_var)
             self.outFile.write(
                 LOAD_CSV.format(var=df_var, pandas_var=PANDAS_VAR, file_name=statement.ds_type.file_name))
+            self.outFile.write(FEATURES.format(x=x, var=df_var, target=statement.ds_type.target))
+            self.outFile.write(TARGET.format(y=y, var=df_var, target=statement.ds_type.target))
+
         elif isinstance(statement, RandomForestStatement):
             clf_var = "clf" + str(curr_count)
             variables = self.emitter.get(statement.parents[0])
             model_type = statement.model_type
-            self.outFile.write(RANDOM_FOREST_INIT.format(var=clf_var, num_trees=model_type.num_trees, criterion=model_type.criterion, max_depth=model_type.max_depth))
+            self.outFile.write(
+                RANDOM_FOREST_INIT.format(var=clf_var, num_trees=model_type.num_trees, criterion=model_type.criterion,
+                                          max_depth=model_type.max_depth))
             self.outFile.write(MODE_FIT.format(var=clf_var, x=variables[0], y=variables[1]))
         elif isinstance(statement, ModelAccuracyStatement):
             self.outFile.write("")
+        self.outFile.write("\n")
--- backend/mlvp/codegen/templates/CodeTemplate.py
+++ backend/mlvp/codegen/templates/CodeTemplate.py
@@ -2,6 +2,8 @@ IMPORT_AS = "import {lib_name} as {lib_var}\n"
 FROM_IMPORT = "from {package} import {class_to_import}\n"
 
 LOAD_CSV = "{var} = {pandas_var}.read_csv(\'./{file_name}\')\n"
+FEATURES = "{x} = {var}.drop(\'{target}\', axis=1)\n"
+TARGET = "{y} = {var}[\'{target}\']\n"
 
 RANDOM_FOREST_INIT = "{var} = RandomForestClassifier(numTrees={num_trees}, criterion=\"{criterion}\", max_depth={max_depth})\n"
-MODE_FIT = "{var}.fit({x},{y})\n"
+MODE_FIT = "{var}.fit({x}, {y})\n"
