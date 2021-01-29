029e8feedf8e2a6f37139c186ca29acd25fc78ad
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-29 14:39:37 +0000

    added over/under-sampling and pca statements

--- backend/mlvp/statement/__init__.py
+++ backend/mlvp/statement/__init__.py
@@ -1,6 +1,8 @@
 from .data.DatasetDeclarationStatement import DatasetDeclarationStatement
 from .data.SplitDatasetStatement import SplitDatasetStatement
-
+from .data.OversamplingStatement import OversamplingStatement
+from .data.UndersamplingStatement import UnderSamplingStatement
+from .data.PCAStatement import PCAStatement
 
 from .model.ModelTrainStatement import ModelTrainStatement
 from .model.RandomForestStatement import RandomForestStatement
