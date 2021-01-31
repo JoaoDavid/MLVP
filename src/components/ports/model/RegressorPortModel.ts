93065a9ea01def4b46e57f1fa06f8a96661cfa48
JoaoDavid <joaodavid256+github@gmail.com>
2021-01-31 16:28:04 +0000

    added regressor and model ports

--- src/components/ports/model/RegressorPortModel.ts
+++ src/components/ports/model/RegressorPortModel.ts
@@ -1,15 +1,15 @@
 import {PortModel} from '@projectstorm/react-diagrams-core';
 import {ModelPortModel} from "./ModelPortModel";
 
-export class ClassifierPortModel extends ModelPortModel {
+export class RegressorPortModel extends ModelPortModel {
 
     constructor(isIn: boolean, name?: string, label?: string, maxLinks?: number){
-        super(isIn, name?name:"Classifier", label?label:"", maxLinks);
+        super(isIn, name?name:"Regressor", label?label:"", maxLinks);
     }
 
     canLinkToPort(port: PortModel): boolean {
-        console.log('canLinkToPort at ClassifierPortModel');
-        if (port instanceof ClassifierPortModel) {
+        console.log('canLinkToPort at RegressorPortModel');
+        if (port instanceof RegressorPortModel) {
             return super.canLinkToPort(port);
         }
         return false;
