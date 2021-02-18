2a6052425602cc29ce51abe778d20e9c6e48b5c3
JoaoDavid <joaodavid256+github@gmail.com>
2021-02-18 21:54:58 +0000

    minor fix on split dataset

--- backend/mlvp/prover/Assertions.py
+++ backend/mlvp/prover/Assertions.py
@@ -48,13 +48,11 @@ def split_dataset(id_input, id_output_train, id_output_test, test_size, train_si
         # Requires
         rows_input >= 2,
         balanced_input,
-        #
+        # falta relacionar os min e max label count
         rows_train == ToInt(ToReal(rows_input) * train_size),
         rows_test == ToInt(ToReal(rows_input) * test_size),
         cols_input == cols_train,
         cols_train == cols_test,
-        n_labels_input == n_labels_train,
-        n_labels_train == n_labels_test,
         n_labels_train == ToInt(ToReal(n_labels_input) * train_size),
         n_labels_test == ToInt(ToReal(n_labels_input) * test_size),
         Implies(stratify, And(balanced_train, balanced_test)),
@@ -132,20 +130,16 @@ def undersampling(id_input, id_output, random_state):
     )
 
 
-# mais de duas rows e mais de 3 colunas
-def func3(id_input):
-    cols = Int(id_input + N_COLS)
-    rows = Int(id_input + N_ROWS)
-    return And(cols > 3, rows > 2)
-
-
 s = Solver()
 s.add(import_from_csv("a", 5, 8, {"batata": 5, "alface": 3}))
 # s.add(split_dataset("b", "c", "d", 0.5, 0.5, False))
 # s.add(oversampling("b", "c", 3))
-s.add(undersampling("b", "c", 3))
 s.add(link("a", "b"))
-if s.check():
+s.add(undersampling("b", "c", 3))
+s.add(link("c", "d"))
+s.add(split_dataset("d", "e", "f", 0.5, 0.5, False, True))
+
+if s.check() == sat:
     m = s.model()
     m_sorted = sorted([str(d) + " = " + str(m[d]) for d in m], key=lambda x: str(x[0]))
     for y in m_sorted:
