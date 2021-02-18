ec97da2d6dc69b1829e7c8b8ca2d798016275872
JoaoDavid <joaodavid256+github@gmail.com>
2021-02-18 11:52:49 +0000

    balaned dataset Bool, oversampling and link fuctions

--- backend/mlvp/prover/Assertions.py
+++ backend/mlvp/prover/Assertions.py
@@ -12,7 +12,15 @@ def import_from_csv(id_output: str, n_cols: int, n_rows: int, labels: Dict[str, 
     label_names = [Int(id_output + "_label_" + key) for key in labels.keys()]
     label_counts = list(labels.values())
     labels_values = [label_names[i] == label_counts[i] for i in range(len(labels))]
-    return And(cols == n_cols, rows == n_rows, And(labels_values))
+    balanced_output = Bool(id_output + "_balanced")
+    list_balanced = [label_counts[i] == label_counts[i+1] for i in range(len(labels)-1)]
+    return And(
+        cols == n_cols,
+        rows == n_rows,
+        rows == sum(label_counts),
+        And(labels_values),
+        balanced_output == And(list_balanced)
+        )
 
 
 def split_dataset(id_input, id_output_train, id_output_test, test_size, train_size, shuffle):
@@ -27,12 +35,33 @@ def split_dataset(id_input, id_output_train, id_output_test, test_size, train_si
     shuffle_output_test = Bool(id_output_test + "_shuffled")
     output_shuffles = Or(shuffle_input, shuffle)
     return And(
+        rows_input >= 2,
         rows_output_train == ToInt(ToReal(rows_input) * train_size),
         rows_output_test == ToInt(ToReal(rows_input) * test_size),
         cols_input == cols_output_train,
         cols_output_train == cols_output_test,
         shuffle_output_train == output_shuffles,
         shuffle_output_test == output_shuffles
+    )
+
+
+def link(id_from: str, id_to: str):
+    cols_from = Int(id_from + "_n_cols")
+    rows_from = Int(id_from + "_n_rows")
+    cols_to = Int(id_to + "_n_cols")
+    rows_to = Int(id_to + "_n_rows")
+    return And(cols_from == cols_to, rows_from == rows_to)
+
+
+def oversampling(id_input, id_output, random_state):
+    cols_input = Int(id_input + "_n_cols")
+    rows_input = Int(id_input + "_n_rows")
+    cols_output = Int(id_output + "_n_cols")
+    rows_output = Int(id_output + "_n_rows")
+    balanced_output = Bool(id_output + "_balanced")
+    return And(
+        cols_input == cols_output,
+        rows_input <= rows_output
     )
 
 
