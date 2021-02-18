ca895475b740da6ca10bce67162b3f0f368f6a89
JoaoDavid <joaodavid256+github@gmail.com>
2021-02-18 13:27:01 +0000

    updated balance output,input

--- backend/mlvp/prover/Assertions.py
+++ backend/mlvp/prover/Assertions.py
@@ -13,14 +13,14 @@ def import_from_csv(id_output: str, n_cols: int, n_rows: int, labels: Dict[str, 
     label_counts = list(labels.values())
     labels_values = [label_names[i] == label_counts[i] for i in range(len(labels))]
     balanced_output = Bool(id_output + "_balanced")
-    list_balanced = [label_counts[i] == label_counts[i+1] for i in range(len(labels)-1)]
+    list_balanced = [label_counts[i] == label_counts[i + 1] for i in range(len(labels) - 1)]
     return And(
         cols == n_cols,
         rows == n_rows,
         rows == sum(label_counts),
         And(labels_values),
         balanced_output == And(list_balanced)
-        )
+    )
 
 
 def split_dataset(id_input, id_output_train, id_output_test, test_size, train_size, shuffle):
@@ -34,8 +34,12 @@ def split_dataset(id_input, id_output_train, id_output_test, test_size, train_si
     shuffle_output_train = Bool(id_output_train + "_shuffled")
     shuffle_output_test = Bool(id_output_test + "_shuffled")
     output_shuffles = Or(shuffle_input, shuffle)
+    balanced_input = Bool(id_input + "_balanced")
     return And(
+        # Requires
         rows_input >= 2,
+        balanced_input,
+        #
         rows_output_train == ToInt(ToReal(rows_input) * train_size),
         rows_output_test == ToInt(ToReal(rows_input) * test_size),
         cols_input == cols_output_train,
@@ -48,9 +52,15 @@ def split_dataset(id_input, id_output_train, id_output_test, test_size, train_si
 def link(id_from: str, id_to: str):
     cols_from = Int(id_from + "_n_cols")
     rows_from = Int(id_from + "_n_rows")
+    balanced_from = Bool(id_from + "_balanced")
     cols_to = Int(id_to + "_n_cols")
     rows_to = Int(id_to + "_n_rows")
-    return And(cols_from == cols_to, rows_from == rows_to)
+    balanced_to = Bool(id_to + "_balanced")
+    return And(
+        cols_from == cols_to,
+        rows_from == rows_to,
+        balanced_from == balanced_to
+    )
 
 
 def oversampling(id_input, id_output, random_state):
@@ -61,7 +71,8 @@ def oversampling(id_input, id_output, random_state):
     balanced_output = Bool(id_output + "_balanced")
     return And(
         cols_input == cols_output,
-        rows_input <= rows_output
+        rows_input <= rows_output,
+        balanced_output
     )
 
 
