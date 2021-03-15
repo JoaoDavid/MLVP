e44f82a24ea3cb39bfa35ca2949615d6b3a23ab5
JoaoDavid <joaodavid256+github@gmail.com>
2021-03-15 19:28:17 +0000

    added link ids to port and link

--- backend/mlvp/ports/ParentLink.py
+++ backend/mlvp/ports/ParentLink.py
@@ -4,6 +4,8 @@ from mlvp.ports.PortType import PortType
 
 class ParentLink:
 
-    def __init__(self, parent_node: Node, parent_source_port: PortType):
+    def __init__(self, link_id: str, parent_node: Node, source_port: PortType, target_port: PortType):
+        self.link_id = link_id
         self.parent_node = parent_node
-        self.parent_source_port = parent_source_port
+        self.source_port = source_port
+        self.target_port = target_port
