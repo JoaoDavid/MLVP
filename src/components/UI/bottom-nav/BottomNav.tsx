772955c0793a7b66e0efc49fca27ff756cae8a0e
JoaoDavid <joaodavid256+github@gmail.com>
2021-03-17 03:09:53 +0000

    displayed in tabs

--- src/components/UI/bottom-nav/BottomNav.tsx
+++ src/components/UI/bottom-nav/BottomNav.tsx
@@ -11,6 +11,7 @@ interface BottomNavProps {
 
 const BottomNav = (props: BottomNavProps) => {
     const tabs = ["Problems", "Assertions", "Log"]
+    const content: JSX.Element[] = [];
     const [activeTab, setActiveTab] = useState(tabs[0]);
 
     const handleClick = (e, newActiveTab) => {
@@ -22,41 +23,68 @@ const BottomNav = (props: BottomNavProps) => {
         }
     };
 
-    const box: JSX.Element[] = [];
-
-
-    let dev = (<span>Project under development</span>);
-    if (process.env.NODE_ENV === 'development') {
-        dev = (<span style={{color: "yellow"}}>DEVELOPMENT VERSION</span>);
-    }
-
-
-    if (props.unsatNodeAssertions.size > 0) {
-        props.unsatNodeAssertions.forEach((problems, node) => {
-            const nodeProblems: JSX.Element[] = [];
-            problems.forEach((problem) => {
-                nodeProblems.push((<div className={classes.Problem}>{problem}</div>));
-            });
-            box.push(<div> {node.getTitle() + " (" + node.getOptions().name + ")"} {nodeProblems} </div>)
+    props.unsatNodeAssertions.forEach((nodeAssertions, node) => {
+        const nodeProblems: JSX.Element[] = [];
+        nodeAssertions.forEach((assertion) => {
+            nodeProblems.push((<li>{assertion}</li>));
         });
-    }
+        content.push(<div>
+            <ul className={classes.list}>
+                <li>{node.getTitle()}</li>
+                <ul className={classes.list}>
+                    {nodeProblems}
+                </ul>
+            </ul>
+        </div>)
+    });
 
-    /*    if (props.allLinkAssertions.size > 0) {
-            props.allLinkAssertions.forEach((problems, link) => {
-                const linkProblems: JSX.Element[] = [];
-                problems.forEach((problem) => {
-                    linkProblems.push((<div className={classes.Problem}>{problem}</div>));
-                });
-                const sourceNode = link.getSourcePort().getNode() as BaseNodeModel;
-                const targetNode = link.getTargetPort().getNode() as BaseNodeModel;
-                box.push(<div> {"Link from " + sourceNode.getTitle() + " to " + targetNode.getTitle()} {linkProblems} </div>)
-            });
-        }*/
+
+    const assertions: JSX.Element[] = [];
+    props.allNodeAssertions.forEach((nodeAssertions, node) => {
+        const nodeProblems: JSX.Element[] = [];
+        nodeAssertions.forEach((assertion) => {
+            nodeProblems.push((<li>{assertion}</li>));
+        });
+        assertions.push(
+            <ul className={classes.list}>
+                <li>{node.getTitle()}</li>
+                <ul className={classes.list}>
+                    {nodeProblems}
+                </ul>
+            </ul>)
+    });
+
+    props.allLinkAssertions.forEach((linkAssertions, link) => {
+        const linkProblems: JSX.Element[] = [];
+        linkAssertions.forEach((assertion) => {
+            linkProblems.push((<li>{assertion}</li>));
+        });
+        const sourceNode = link.getSourcePort().getNode() as BaseNodeModel;
+        const targetNode = link.getTargetPort().getNode() as BaseNodeModel;
+        assertions.push(
+            <ul>
+                <li>{"Link from " + sourceNode.getTitle() + " to " + targetNode.getTitle()}</li>
+                <ul>
+                    {linkProblems}
+                </ul>
+            </ul>)
+    });
+
+    content.push(<div>{assertions}</div>)
+
+    let current_content = null;
+    if(activeTab == tabs[0]) {
+        current_content = content[0];
+    } else if (activeTab == tabs[1]) {
+        current_content = content[1];
+    } else {
+        current_content = (<div></div>)
+    }
 
     return (
         <div className={classes.Unselectable}>
-            <div className={classes.content}>
-                {box}
+            <div className={classes.Box}>
+                {current_content}
             </div>
             <div className={classes.BottomNav}>
                 <ul className={classes.tabs}>
