2d305585c74a46d66af3a9ec396f63ee687f033e
JoaoDavid <joaodavid256+github@gmail.com>
2021-03-19 23:55:07 +0000

    FC undersampling

--- src/components/nodes/data/undersampling/UndersamplingWidget.tsx
+++ src/components/nodes/data/undersampling/UndersamplingWidget.tsx
@@ -10,29 +10,18 @@ interface UndersamplingProps {
     engine: DiagramEngine;
 }
 
-type UndersamplingState = {
-    node: UndersamplingModel;
-};
+const UndersamplingWidget = (props: UndersamplingProps) => {
 
-class UndersamplingWidget extends React.Component<UndersamplingProps, UndersamplingState> {
-
-    state = {
-        node: this.props.node,
+    const randomStateChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.node.setRandomState(+event.target.value);
     }
 
-    randomStateChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.state.node.setRandomState(+event.target.value);
-        this.setState({});
-    }
-
-    render() {
-        const modal = <UndersamplingModal node={this.props.node} randomStateChanged={this.randomStateChanged}/>;
-        return (
-            <BaseNodeWidget node={this.props.node} engine={this.props.engine} color={DATA_CONFIG.color} modalChildren={modal}>
-                <p>Random State: {this.state.node.getRandomState()}</p>
-            </BaseNodeWidget>
-        );
-    }
+    const modal = <UndersamplingModal node={props.node} randomStateChanged={randomStateChanged}/>;
+    return (
+        <BaseNodeWidget node={props.node} engine={props.engine} color={DATA_CONFIG.color} modalChildren={modal}>
+            <p>Random State: {props.node.getRandomState()}</p>
+        </BaseNodeWidget>
+    );
 
 }
 
