34ad88150ada6cc484dd93a80dfcd7fcdfc06521
JoaoDavid <joaodavid256+github@gmail.com>
2021-03-29 22:23:57 +0100

    added codegen and import methods

--- backend/mlvp/ast/nodes/data/AbstractDataset.py
+++ backend/mlvp/ast/nodes/data/AbstractDataset.py
@@ -1,4 +1,5 @@
 from mlvp.ast.nodes.Node import Node
+from mlvp.codegen import Emitter
 from mlvp.typecheck import *
 
 
@@ -8,6 +9,17 @@ class AbstractDataset(Node):
         super().__init__(data)
         self.num_cols = data['numCols']
         self.num_rows = data['numRows']
+
+    def import_dependency(self):
+        return ""
+
+    def codegen(self, emitter: Emitter, out_file):
+        curr_count = emitter.get_count()
+        x = "x" + str(curr_count)
+        y = "y" + str(curr_count)
+        out_ds = self.get_port(False, "Dataset")
+        emitter.set(out_ds, (x, y))
+        out_file.write("AbstractDataset does not have a codegen implementation\n")
 
     def assertions(self):
         out_ds = self.get_port(False, "Dataset").port_id
