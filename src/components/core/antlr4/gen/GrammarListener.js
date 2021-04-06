34601166dc5ddd54784877e3edf9686f1cc2daf2
JoaoDavid <joaodavid256+github@gmail.com>
2021-04-06 23:31:38 +0100

    added generated antlr files

---
+++ src/components/core/antlr4/gen/GrammarListener.js
@@ -0,0 +1,54 @@
+// Generated from /home/david/Documents/git/mlvp/src/components/core/antlr4/Grammar.g4 by ANTLR 4.9.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by GrammarParser.
+export default class GrammarListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by GrammarParser#program.
+	enterProgram(ctx) {
+	}
+
+	// Exit a parse tree produced by GrammarParser#program.
+	exitProgram(ctx) {
+	}
+
+
+	// Enter a parse tree produced by GrammarParser#statement.
+	enterStatement(ctx) {
+	}
+
+	// Exit a parse tree produced by GrammarParser#statement.
+	exitStatement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by GrammarParser#create_col_stat.
+	enterCreate_col_stat(ctx) {
+	}
+
+	// Exit a parse tree produced by GrammarParser#create_col_stat.
+	exitCreate_col_stat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by GrammarParser#expr.
+	enterExpr(ctx) {
+	}
+
+	// Exit a parse tree produced by GrammarParser#expr.
+	exitExpr(ctx) {
+	}
+
+
+	// Enter a parse tree produced by GrammarParser#unary_ope.
+	enterUnary_ope(ctx) {
+	}
+
+	// Exit a parse tree produced by GrammarParser#unary_ope.
+	exitUnary_ope(ctx) {
+	}
+
+
+
+}
