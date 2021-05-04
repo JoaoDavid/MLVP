9f007fd2de0b5dfd2b8fd9423fa5c49aef01a6a2
JoaoDavid <joaodavid256+github@gmail.com>
2021-05-04 18:10:11 +0100

    implemented node class creator script

---
+++ create-node.py
@@ -0,0 +1,65 @@
+import sys
+import os
+
+CATEGORY_CONFIG = "CATEGORY_CONFIG"
+NODE_CONFIG = "TEMPLATE_CONFIG_VAR"
+NODE_CODENAME = "TemplateCodeName"
+NODE_NAME = "TemplateName"
+
+
+def create_node(input_fp, output_fp, category, name, code_name, node_config_var):
+    print(input_fp)
+    # print(file_name)
+    input_file = open(input_fp, "r")
+    # name = os.path.basename(path)
+
+    output_file = open(output_fp, "w")
+
+    text = input_file.read()
+    text = text.replace(CATEGORY_CONFIG, category + "_CONFIG")
+    text = text.replace(NODE_CONFIG, node_config_var)
+    text = text.replace(NODE_CODENAME, code_name)
+    text = text.replace(NODE_NAME, name)
+
+    output_file.write(text)
+    output_file.close()
+
+
+def main():
+    category = sys.argv[1].replace(" ", "_")
+    category = category.upper()
+    print("category: " + category)
+
+    code_name = sys.argv[2].replace(" ", "")
+    node_config_var = sys.argv[2].replace(" ", "_")
+    node_config_var = node_config_var.upper()
+
+    print("name: " + sys.argv[2])
+    print("code_name: " + code_name)
+    print("node_config_var: " + node_config_var)
+
+    template_directory = './node-creator'
+    category_ts = category.lower().replace("_", "-")
+    node_folder_ts = node_config_var.lower().replace("_", "-")
+    output_directory_ts = "./src/components/nodes/" + category_ts + "/" + node_folder_ts
+    category_ts = category.lower()
+    output_directory_py = "./backend/mlvp/graph/nodes/" + category_ts
+    if not os.path.exists(output_directory_ts):
+        os.makedirs(output_directory_ts)
+    if not os.path.exists(output_directory_py):
+        os.makedirs(output_directory_py)
+    # iterate over files in
+    # that directory
+    for file_name in os.listdir(template_directory):
+        input_fp = os.path.join(template_directory, file_name)
+        if ".py" in file_name:
+            output_fp = output_directory_py + "/" + code_name + file_name
+        else:
+            output_fp = output_directory_ts + "/" + code_name + file_name
+        # checking if it is a file
+        if os.path.isfile(input_fp):
+            create_node(input_fp, output_fp, category, sys.argv[2], code_name, node_config_var)
+
+
+if __name__ == "__main__":
+    main()
---
+++ node-creator/Model.ts
@@ -0,0 +1,57 @@
+import {BaseNodeModel, NodeConfig} from "../../../core/BaseNode/BaseNodeModel";
+import {DatasetPortModel} from "../../../ports/dataset/DatasetPortModel";
+import {DeserializeEvent} from "@projectstorm/react-canvas-core";
+
+export const TEMPLATE_CONFIG_VAR: NodeConfig = {
+    codeName: "TemplateCodeName",
+    name: "TemplateName",
+}
+
+
+export class TemplateCodeNameModel extends BaseNodeModel {
+
+    constructor() {
+        super(TEMPLATE_CONFIG_VAR);
+        this.addInPort();
+        this.addOutPort();
+    }
+
+    updateNode = () => {
+        // TODO
+        let colMap = this.getColumnsAndTypes();
+        let listColNames = Object.keys(colMap);
+    }
+
+    protected addInPort(): void {
+        // TODO
+        const p = new DatasetPortModel(true);
+        super.addPort(p);
+    }
+
+    protected addOutPort(): void {
+        // TODO
+        const p = new DatasetPortModel(false, "Engineered Dataset");
+        super.addPort(p);
+    }
+
+    deserialize(event: DeserializeEvent<this>) {
+        super.deserialize(event);
+        // TODO
+/*        this.newColumnName = event.data.column_name;
+        this.originalColumnName = event.data.originalColumnName;
+        this.metric = event.data.metric;
+        this.windowSize = event.data.windowSize;*/
+    }
+
+    serialize(): any {
+        return {
+            ...super.serialize(),
+            // TODO
+/*            newColumnName: this.newColumnName,
+            originalColumnName: this.originalColumnName,
+            metric: this.metric,
+            windowSize: this.windowSize,*/
+        };
+    }
+
+}
