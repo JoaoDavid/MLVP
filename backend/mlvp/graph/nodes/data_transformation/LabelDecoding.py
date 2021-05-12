49a7fb52f5bda04efef5ecda8ac9ba59357c48ef
JoaoDavid <joaodavid256+github@gmail.com>
2021-05-12 13:15:03 +0100

    added column exists assertion

--- backend/mlvp/graph/nodes/data_transformation/LabelDecoding.py
+++ backend/mlvp/graph/nodes/data_transformation/LabelDecoding.py
@@ -8,7 +8,7 @@ RENAME_COLUMN = "{df} = {df}.rename(columns={{'{encoded_column}': '{original_col
 X = "{x} = {df}.drop({old_y}.name, axis=1)\n"
 Y = "{y} = {df}[{old_y}.name]\n"
 
-# NONEXISTENT_COLUMN = "column \"{column_name}\" exists on the dataset"
+NONEXISTENT_COLUMN = "column \"{column_name}\" exists on the dataset"
 DUPLICATE_COLUMN = "column \"{column_name}\" is unique on the dataset"
 
 
@@ -69,15 +69,15 @@ class LabelDecoding(Node):
         self.data_flow(node_columns)
 
         z3_duplicate_column = Bool(DUPLICATE_COLUMN.format(column_name=self.original_column))
-        # z3_nonexistent_column = Bool(NONEXISTENT_COLUMN.format(column_name=self.encoded_column))
+        z3_nonexistent_column = Bool(NONEXISTENT_COLUMN.format(column_name=self.encoded_column))
 
         aux_assertions = []
         if len(input_port.columns) > 0:
             duplicate_column = self.original_column not in input_port.columns
             nonexistent_column = self.encoded_column in input_port.columns
             aux_assertions = [
-                # z3_nonexistent_column == nonexistent_column,
-                # z3_nonexistent_column,
+                z3_nonexistent_column == nonexistent_column,
+                z3_nonexistent_column,
                 z3_duplicate_column == duplicate_column,
                 z3_duplicate_column,
                 column(input_ds.dataset, String(self.encoded_column)) == get_col_type("int"),
--- backend/mlvp/graph/nodes/data_transformation/TemporalAggregation.py
+++ backend/mlvp/graph/nodes/data_transformation/TemporalAggregation.py
@@ -8,7 +8,7 @@ X = "{x} = {df}.drop({old_y}.name, axis=1)\n"
 Y = "{y} = {df}[{old_y}.name]\n"
 PANDAS_VAR = "pd"
 
-# NONEXISTENT_COLUMN = "column \"{column_name}\" exists on the dataset"
+NONEXISTENT_COLUMN = "column \"{column_name}\" exists on the dataset"
 DUPLICATE_COLUMN = "column \"{column_name}\" is unique on the dataset"
 
 
@@ -67,11 +67,15 @@ class TemporalAggregation(Node):
 
         z3_len_new_col = Int(NODE_PROP.format(name="len_new_column_name", node_id=self.node_id))
         z3_duplicate_column = Bool(DUPLICATE_COLUMN.format(column_name=self.new_col_name))
+        z3_nonexistent_column = Bool(NONEXISTENT_COLUMN.format(column_name=self.original_column))
 
         assert_existent_column = []
         if len(input_port.columns) > 0:
             duplicate_column = self.new_col_name not in input_port.columns
+            nonexistent_column = self.original_col_name in input_port.columns
             assert_existent_column = [
+                z3_nonexistent_column == nonexistent_column,
+                z3_nonexistent_column,
                 z3_duplicate_column == duplicate_column,
                 z3_duplicate_column,
                 Or(
