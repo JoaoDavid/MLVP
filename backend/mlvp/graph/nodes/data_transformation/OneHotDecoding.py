567f19aed008452c1b5a5e9d8ba36b17393ce6a7
JoaoDavid <joaodavid256+github@gmail.com>
2021-05-13 16:31:51 +0100

    updated with new column type one-hot-encoded

--- backend/mlvp/graph/nodes/data_transformation/OneHotDecoding.py
+++ backend/mlvp/graph/nodes/data_transformation/OneHotDecoding.py
@@ -3,13 +3,15 @@ from mlvp.graph.nodes.Node import *
 from mlvp.typecheck import *
 
 CONCATENATE = "{df} = pd.concat([{old_x},{old_y}], join = 'outer', axis = 1)\n"
+DUMMIES = "{dummies} = {df}[{unique_labels}]\n"
 DECODED = "{decoded} = pd.Series({dummies}.columns[np.where({dummies}!=0)[1]])\n"
-INDEX = "{index} = {df}.columns.get_loc(\'{category}\')\n"
-INSERT = "{df}.insert(loc={index}, column=\'{encoded_column}\', value={decoded})\n"
-DROP = "{df} = {df}.drop(columns={categories})\n"
-X = "{x} = {df}.drop({old_y}.name, axis=1)\n"
-Y = "{y} = {df}[{old_y}.name]\n"
+INDEX = "{index} = {df}.columns.get_loc({unique_labels}[0])\n"
+INSERT = "{df}.insert(loc={index}, column=\'{original_column}\', value={decoded})\n"
+DROP = "{df} = {df}.drop(columns={unique_labels})\n"
+X = "{x} = {df}.drop({df}.columns[-1], axis=1)\n"
+Y = "{y} = {df}[{df}.columns[-1]]\n"
 
+NONEXISTENT_COLUMN = "column \"{column_name}\" exists on the dataset"
 DUPLICATE_COLUMN = "column \"{column_name}\" is unique on the dataset"
 
 
@@ -18,7 +20,7 @@ class OneHotDecoding(Node):
     def __init__(self, data):
         super().__init__(data)
         self.encoded_column = data['encodedColumn']
-        self.categories = []
+        self.original_column = self.encoded_column.replace("_one_hot_encoded", "")
         self.decodable_columns = {}
 
     def import_dependency(self):
@@ -28,20 +30,22 @@ class OneHotDecoding(Node):
         curr_count = emitter.get_count()
         parent_port = self.parent_links[0].source_port
         old_x, old_y = emitter.get(parent_port)
-        dummies = emitter.get(self.encoded_column)
+        unique_labels = emitter.get(self.encoded_column)
+        dummies = "dummies" + str(curr_count)
         decoded = "decoded" + str(curr_count)
         index = "index" + str(curr_count)
         df = "df" + str(curr_count)
         x = "x" + str(curr_count)
         y = "y" + str(curr_count)
-        print(self.categories)
+
         out_file.write(CONCATENATE.format(df=df, old_x=old_x, old_y=old_y))
+        out_file.write(DUMMIES.format(dummies=dummies, df=df, unique_labels=unique_labels))
         out_file.write(DECODED.format(decoded=decoded, dummies=dummies))
-        out_file.write(INDEX.format(index=index, df=df, category=self.categories[0]))
-        out_file.write(INSERT.format(df=df, index=index, encoded_column=self.encoded_column, decoded=decoded))
-        out_file.write(DROP.format(df=df, categories=self.categories))
-        out_file.write(X.format(x=x, df=df, old_y=old_y))
-        out_file.write(Y.format(y=y, df=df, old_y=old_y))
+        out_file.write(INDEX.format(index=index, df=df, unique_labels=unique_labels))
+        out_file.write(INSERT.format(df=df, index=index, original_column=self.original_column, decoded=decoded))
+        out_file.write(DROP.format(df=df, unique_labels=unique_labels))
+        out_file.write(X.format(x=x, df=df))
+        out_file.write(Y.format(y=y, df=df))
 
         out_ds = self.get_port(False, "Decoded Dataset")
         emitter.set(out_ds, (x, y))
@@ -53,17 +57,16 @@ class OneHotDecoding(Node):
         output_port.encoded_columns = input_port.encoded_columns.copy()
 
         for col_name, tuple_encoded in input_port.encoded_columns.items():
-            if tuple_encoded[0] == "one-hot-encoded":
+            if tuple_encoded[0] == "one_hot_encoded":
                 self.decodable_columns[col_name] = tuple_encoded[2]
         node_columns[self.node_id] = self.decodable_columns
 
         if self.encoded_column in input_port.encoded_columns:
-            self.categories = input_port.categories[self.encoded_column]
             del output_port.encoded_columns[self.encoded_column]
 
         for col_name, col_type in input_port.columns.items():
-            if col_name in self.categories:
-                output_port.columns[self.encoded_column] = "string"
+            if col_name == self.encoded_column:
+                output_port.columns[self.original_column] = input_port.encoded_columns[self.encoded_column][1]
             else:
                 output_port.columns[col_name] = col_type
 
@@ -73,17 +76,23 @@ class OneHotDecoding(Node):
         input_ds = Dataset(input_port.port_id)
         output_ds = Dataset(output_port.port_id)
 
+        z3_duplicate_column = Bool(DUPLICATE_COLUMN.format(column_name=self.original_column))
+        z3_nonexistent_column = Bool(NONEXISTENT_COLUMN.format(column_name=self.encoded_column))
+
         aux_assertions = []
         if len(input_port.columns) > 0:
-            if self.encoded_column in input_port.encoded_columns:
-                z3_duplicate_column = Bool(DUPLICATE_COLUMN.format(column_name=self.encoded_column))
-                aux_assertions.append(z3_duplicate_column == (self.encoded_column not in input_port.columns))
-                aux_assertions.append(z3_duplicate_column)
-
+            duplicate_column = self.original_column not in input_port.columns
+            nonexistent_column = self.encoded_column in input_port.columns
+            aux_assertions = [
+                z3_nonexistent_column == nonexistent_column,
+                z3_nonexistent_column,
+                z3_duplicate_column == duplicate_column,
+                z3_duplicate_column,
+                # column(input_ds.dataset, String(self.encoded_column)) == get_col_type("int"),
+            ]
             if len(self.decodable_columns) == 0:
-                z3_num_encoded_cols = Int(NODE_PROP.format(name="num_decodable_columns", node_id=self.node_id))
-                encoded_cols = len(self.decodable_columns)
-                aux_assertions.append(z3_num_encoded_cols == encoded_cols)
+                z3_num_encoded_cols = Int(NODE_PROP.format(name="num_encoded_cols", node_id=self.node_id))
+                aux_assertions.append(z3_num_encoded_cols == len(self.decodable_columns))
                 aux_assertions.append(z3_num_encoded_cols > 0)
 
         return [
