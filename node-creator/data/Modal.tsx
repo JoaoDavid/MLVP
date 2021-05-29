4f5ca91a802e2eabab1db3fcfe4e370a0eb3d403
JoaoDavid <joaodavid256+github@gmail.com>
2021-05-29 22:39:16 +0100

    removed unused imports

--- node-creator/data/Modal.tsx
+++ node-creator/data/Modal.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import InputGroup from "react-bootstrap/InputGroup";
 import {TemplateCodeNameModel} from "./TemplateCodeNameModel";
-import classes from "../../../UI/modal/BaseModal.module.css";
 
 
 interface TemplateCodeNameModalProps {
