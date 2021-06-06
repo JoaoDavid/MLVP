fca8990cbf62ad49da22fa9e31a92b5946e5c233
JoaoDavid <joaodavid256+github@gmail.com>
2021-06-06 15:40:59 +0100

    updated variable names and comments

--- backend/mlvp/typecheck/Linking.py
+++ backend/mlvp/typecheck/Linking.py
@@ -2,20 +2,20 @@ from z3 import *
 
 
 def link(id_source_port: str, id_target_port: str):
-    id_source_port = Dataset(id_source_port)
-    port_to = Dataset(id_target_port)
+    source = Dataset(id_source_port)
+    target = Dataset(id_target_port)
 
     return [
-        id_source_port.cols == port_to.cols,
-        id_source_port.rows == port_to.rows,
-        id_source_port.balanced == port_to.balanced,
-        id_source_port.time_series == port_to.time_series,
-        id_source_port.dataset == port_to.dataset,
-        id_source_port.n_labels == port_to.n_labels,
-        id_source_port.max_label_count == port_to.max_label_count,
-        id_source_port.min_label_count == port_to.min_label_count,
-        id_source_port.reduced == port_to.reduced,
-        id_source_port.processed == port_to.processed,
+        source.cols == target.cols,
+        source.rows == target.rows,
+        source.balanced == target.balanced,
+        source.time_series == target.time_series,
+        source.dataset == target.dataset,
+        source.n_labels == target.n_labels,
+        source.max_label_count == target.max_label_count,
+        source.min_label_count == target.min_label_count,
+        source.reduced == target.reduced,
+        source.processed == target.processed,
     ]
 
 
