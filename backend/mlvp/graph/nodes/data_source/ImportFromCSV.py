e09c7677ac3d2014fd1f426424a0ca29bb6ec7e9
JoaoDavid <joaodavid256+github@gmail.com>
2021-07-01 19:00:37 +0100

    fixed num rows assertion

--- backend/mlvp/graph/nodes/data_source/ImportFromCSV.py
+++ backend/mlvp/graph/nodes/data_source/ImportFromCSV.py
@@ -80,14 +80,13 @@ class ImportFromCSV(Node):
         return [
                    output.cols == self.num_cols,
                    output.rows == self.num_rows,
-                   output.rows == sum(label_counts),
                    output.time_series == self.time_series,
                    output.reduced == False,
                    output.processed == False,
-            # And(labels_values),
+                   # And(labels_values),
                    output.balanced == is_balanced,
-            # output.balanced == And(list_balanced),
+                   # output.balanced == And(list_balanced),
                    output.n_labels == len(label_counts),
                    z3_unique_col_names == unique_col_names,
-            z3_unique_col_names,
-        ] + col_assertions
+                   z3_unique_col_names,
+               ] + col_assertions
